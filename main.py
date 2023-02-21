478e2e736f9e2507e0fedca4401d3b03b7d4bf1f
Ivan Chernyakov <3699818@mail.ru>
2023-02-21 13:43:32 +0300

    Add subprocess and flight find endpoint

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import shutil
+import subprocess
 
+from bson import ObjectId
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 import motor.motor_asyncio
 
 from models import FlightReport
@@ -32,6 +35,22 @@ async def upload_file(images: list[UploadFile] = File(...)):
 
     flight = {'path_to_images': 'uploads'}
     new_flight = await db["flight-reports"].insert_one(flight)
+
     created_flight = await db["flight-reports"].find_one({"_id": new_flight.inserted_id})
+    subprocess.Popen([
+        'python3',
+        '/Users/ivancernakov/Documents/metashape-orthomosaic-maker/main.py',
+        '--flight_id',
+        str(new_flight.inserted_id),
+        '--image_dir',
+        '/Users/ivancernakov/Documents/metashape-orthomosaic-maker/images'
+    ])
 
     return created_flight
+
+
+@app.get("/{flight_id}")
+async def upload_file(flight_id: str):
+    flight = await db["flight-reports"].find_one({"_id": ObjectId(flight_id)})
+
+    return JSONResponse(status_code=200, content=flight['orthomosaic_progress'])
